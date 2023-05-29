ccde155175b3382a9b107abcd918661d55cebcb3
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-05-29 15:54:25 +0800

    config

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 // localhost
-$server = "localhost";
-$server_username = "root";
-$server_password = "";
-$server_database = "shop_db";
-$home = "http://localhost/trylang/";
+// $server = "localhost";
+// $server_username = "root";
+// $server_password = "";
+// $server_database = "shop_db";
+// $home = "http://localhost/trylang/";
 
 // //hosting
-// $server = "localhost";
-// $server_username = "u235219407_rwstrucking";
-// $server_password = "Nichole@15";
-// $server_database = "u235219407_rwstrucking";
-// $home = "https://ridealong.dnails.shop//";
+$server = "localhost";
+$server_username = "u235219407_rwstrucking";
+$server_password = "Nichole@15";
+$server_database = "u235219407_rwstrucking";
+$home = "https://ridealong.dnails.shop//";
 
 $conn = mysqli_connect($server, $server_username, $server_password, $server_database);
 session_start();
